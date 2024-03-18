3fae434cf93c35d4f6eea1b2ad74bf685627f9f5
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-18 16:41:33 +0000

    edited php with target

--- uploadCSV.php
+++ uploadCSV.php
@@ -1,3 +1,4 @@
 <?php>
-$deliveryinfo .= "('".$_POST['lastName']."', '".$_POST['firstName']."', '".$_POST['balesOrdered']."','".$_POST['streetNumber']."', '".$_POST['streetName']."', '".$_POST['city']."', '".$_POST['state']."', '".$_POST['zip']."', '")
+$target_dir = "deliveryinfo" 
+"('".$_POST['lastName']."', '".$_POST['firstName']."', '".$_POST['balesOrdered']."','".$_POST['streetNumber']."', '".$_POST['streetName']."', '".$_POST['city']."', '".$_POST['state']."', '".$_POST['zip']."', '")
 ?>
