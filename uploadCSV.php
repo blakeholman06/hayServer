f34e30debf1b5be24732e9a2a66a0215ef3d340d
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-18 16:58:40 +0000

    Joe's php

--- uploadCSV.php
+++ uploadCSV.php
@@ -1,4 +1,24 @@
-<?php>
-$target_dir = "hayBaleData/deliveryinfo" 
-"('".$_POST['lastName']."', '".$_POST['firstName']."', '".$_POST['balesOrdered']."','".$_POST['streetNumber']."', '".$_POST['streetName']."', '".$_POST['city']."', '".$_POST['state']."', '".$_POST['zip']."', '")
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "hay";
+$dbname = "hayBaleData";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO deliveryInfo (lastName, firstName, streetNumber, streetName, city, state, zip,)
+VALUES ('$_POST[lastName]', '$_POST[firstName]', '$_POST[streetNumber]', '$_POST[streetName]', '$_POST[city]', '$_POST[state]', '$_POST[zip]')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
