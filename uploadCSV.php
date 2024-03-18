a7e02965a0b53495000acdcc2790efe446fe60c5
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-18 16:37:29 +0000

    potential php

---
+++ uploadCSV.php
@@ -0,0 +1,3 @@
+<?php>
+$deliveryinfo .= "('".$_POST['lastName']."', '".$_POST['firstNamw']."', '".$_POST['balesOrdered']."','".$_POST['streetNumber']."', '".$_POST['streetName']."', '".$_POST['city']."', '".$_POST['state']."', '".$_POST['zip']."', '")
+?>
