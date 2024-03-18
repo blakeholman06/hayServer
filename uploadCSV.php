f54cc25056e4b45b90eab66e867b108f8808c82f
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-18 16:44:25 +0000

    changed directory of php

--- uploadCSV.php
+++ uploadCSV.php
@@ -1,4 +1,4 @@
 <?php>
-$target_dir = "deliveryinfo" 
+$target_dir = "hayBaleData/deliveryinfo" 
 "('".$_POST['lastName']."', '".$_POST['firstName']."', '".$_POST['balesOrdered']."','".$_POST['streetNumber']."', '".$_POST['streetName']."', '".$_POST['city']."', '".$_POST['state']."', '".$_POST['zip']."', '")
 ?>
