cdb5744f178e0ab624e8b44550420f482fd4c1c3
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-21 16:39:48 +0000

    added sql to insert

--- uploadCSV.php
+++ uploadCSV.php
@@ -17,7 +17,7 @@ $streetName = $_POST['streetName'];
 $city = $_POST['city'];
 $state = $_POST['state'];
 $zip = $_POST['zip'];
-INSERT INTO `deliveryinfo`(`lastName`, `firstName`, `balesOrdered`, `streetNumber`, `streetName`, `city`, `state`, `zip`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]')
+$sql = INSERT INTO `deliveryinfo`(`lastName`, `firstName`, `balesOrdered`, `streetNumber`, `streetName`, `city`, `state`, `zip`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]')
 $sql = "SELECT lastName, firstName, balesOrdered, streetNumber, streetName, city, state, zip FROM 'deliveryinfo';";
 $result = mysqli_query($conn, $sql);
 
