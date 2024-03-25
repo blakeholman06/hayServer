8d80e6a4307b9975217de5401598c2660f567002
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-25 16:34:07 +0000

    added sql

--- uploadCSV.php
+++ uploadCSV.php
@@ -14,12 +14,12 @@ die("connection failed, womp womp " . mysqli_connect_error());
 $sql = "SELECT lastName, firstName, balesOrdered, streetNumber, streetName, city, state, zip FROM 'deliveryinfo';";
 $result = mysqli_query($conn, $sql);
 
-INSERT INTO deliveryinfo (lastName, firstName, balesOrdered, streetNumber, city, state, zip)
-VALUES ('Radaker', 'Joseph', '30', '22 Jameston Dr', 'Jefferson', 'GA', '30549');
+$sql = "INSERT INTO deliveryinfo (lastName, firstName, balesOrdered, streetNumber, city, state, zip)
+VALUES ('Radaker', 'Joseph', '30', '22 Jameston Dr', 'Jefferson', 'GA', '30549')";
 
 if (mysqli_num_rows($result) > 0)  {
   //handle output
-  while($row = mysqli_fetch_assoc($result))  {
+  while($row = mysqli_fetch_assoc($result))   {
     echo "lastName: " . $row["lastName"];
     echo "firstName: " . $row["firstName"];
     echo "balesOrdered: " . $row["balesOrdered"];
