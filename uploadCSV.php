30d5182f2ff0f19ca578919842cb885cf866b805
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-26 16:18:16 +0000

    trying again with "

--- uploadCSV.php
+++ uploadCSV.php
@@ -17,8 +17,7 @@ $streetName = "Main St";
 $city = "Anytown";
 $state = "CA";
 $zip = "12345";
-$sql = "INSERT INTO deliveryinfo (lastName, firstName, balesOrdered, streetNumber, streetName, city, state, zip)"
-VALUES ('$lastName', '$firstName', '$balesOrdered', '$streetNumber', '$streetName', '$city', '$state', '$zip')";
+$sql = "INSERT INTO deliveryinfo (lastName, firstName, balesOrdered, streetNumber, streetName, city, state, zip) VALUES ('$lastName', '$firstName', '$balesOrdered', '$streetNumber', '$streetName', '$city', '$state', '$zip')";
   
 $sql = "SELECT lastName, firstName, balesOrdered, streetNumber, streetName, city, state, zip FROM 'deliveryinfo'";
 $result = mysqli_query($conn, $sql);
