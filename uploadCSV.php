969897e241db87f2044921201a51d3582472ab35
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-26 17:18:20 +0000

    added select

--- uploadCSV.php
+++ uploadCSV.php
@@ -26,7 +26,29 @@ if (mysqli_query($conn, $sql)) {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 //to make a save where everything is ok
+
+$sql = "SELECT lastName, firstName, balesOrdered, streetNumber, streetName, city, state, zip FROM 'deliveryinfo'";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0)  {
+    //handle output
+    while($row = mysqli_fetch_assoc($result))   {
+      echo "lastName: " . $row["lastName" ];
+      echo "firstName: " . $row["firstName"];
+      echo "balesOrdered: " . $row["balesOrdered"];
+      echo "streetNumber: " . $row["streetNumber"];
+      echo "streetName: " . $row["streetName"];
+      echo "city: " . $row["city"];
+      echo "state: " . $row["state"];
+      echo "zip: " . $row["zip"];
+
+    }
+  } else {
+    echo "no records found womp womp";
+  }
+
 myqli_close($conn);
+
 
 
 
