9045f461fd372b308947dde13a319e7c0b5fd35b
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-03-28 16:59:10 +0000

    trying joes code

--- updateCSV.php
+++ updateCSV.php
@@ -7,7 +7,7 @@ $dbname = "hayBaleData";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if(!$conn) {
-die("connection failed, womp womp " . mysqli_connect_error());
+die("connection failed, womp womp" . mysqli_connect_error());
 }
 $lastName = "Radaker";
 $firstName = "Joe";
@@ -17,13 +17,14 @@ $streetName = "Main St";
 $city = "Anytown";
 $state = "CA";
 $zip = "12345";
-$sql = "UPDATE  deliveryinfo SET balesOrdered = $balesOrdered + 100 WHERE id = 1";
+$sql = "UPDATE  deliveryinfo SET balesOrdered = '$balesOrdered' + 100 WHERE id = 1";
 
 if (mysqli_query($conn, $sql)) {
   echo "Record updated successfully";
 } else {
   echo "Error updating record: " . mysqli_error($conn);
 }
+
 mysqli_close($conn);
 
 
