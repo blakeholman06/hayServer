132c0abab2d740b92042fa5858ed06840430a0f8
Blake Holman <88340231+blakeholman06@users.noreply.github.com>
2024-04-12 16:09:31 +0000

    got rid of where in read

--- readCSV.php
+++ readCSV.php
@@ -18,7 +18,7 @@ $city = "Anytown";
 $state = "CA";
 $zip = "12345";
 
-$sql = "SELECT `lastName`, `firstName`, `balesOrdered`, `streetNumber`, `streetName`, `city`, `state`, `zip`, `id` FROM `deliveryinfo` WHERE 0";
+$sql = "SELECT `lastName`, `firstName`, `balesOrdered`, `streetNumber`, `streetName`, `city`, `state`, `zip`, `id` FROM `deliveryinfo`";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0)  {
